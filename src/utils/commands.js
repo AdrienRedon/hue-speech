a821a977f83e53c82bc77255698bf3680a317335
Adrien REDON <redon.adrien@gmail.com>
2017-02-16 08:20:18 +0100

    REF: Functionnalize code & improve quality 😎

--- src/utils/commands.js
+++ src/utils/commands.js
@@ -2,53 +2,34 @@
 import axios from 'axios';
 import config from '../../config.json';
 
-const _actions = {
-    turnOn: [
-        'allume',
-    ],
-    turnOff: [
-        'étein',
-    ],
-};
-
-const _lightOn = {"on":true, "bri": 254, "sat": 0,"hue":0};
-const _lightOff = {"on":false, "bri": 0, "sat": 0,"hue":0};
-
-const _turnOn =  (lights) => {
-    lights.forEach((light, index) => {
-        if (light) 
-            axios.put(`${config.url}/lights/${index}/state`, _lightOn);
-    });
-};
-
-const _turnOff = (lights) => {
-    lights.forEach((light, index) => {
-        if (light) 
-            axios.put(`${config.url}/lights/${index}/state`, _lightOff);
-    });
-};
-
 const getActions = (text) => {
-    const isTurnOn = _actions.turnOn.reduce((total, action) => total + text.includes(action), 0);
-    const isTurnOff = _actions.turnOff.reduce((total, action) => total + text.includes(action), 0);
-    return {
-        isTurnOn, 
-        isTurnOff,
-    };
+    return config.actions.map(action => {
+        const triggered = action.keywords.reduce((total, keyword) => {
+            return total + text.includes(keyword)
+        }, 0);
+
+        return {
+            name: action.name,
+            triggered
+        };
+    });
 };
 
 const getLights = (text) => {
     return config.lights.map(light => {
-        return light.keywords.reduce((total, keyword) => total + text.includes(keyword), 0);
+        return light.keywords.reduce((totalLight, keyword) => totalLight + text.includes(keyword), 0);
     });
 };
 
 const send = (actions, lights) => {
-    if (actions.isTurnOn) {
-        _turnOn(lights);
-    } else if (actions.isTurnOff) {
-        _turnOff(lights);
-    }
+    actions.forEach((action, indexAction) => {
+        console.log(action);
+        if (action.triggered) 
+            lights.forEach((light, indexLight) => {
+                if (light) 
+                    axios.put(`${config.url}/lights/${indexLight + 1}/state`, config.actions.find(a => a.name === action.name).data);
+            });
+    });
 };
 
 export default {
